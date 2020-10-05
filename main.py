7d0e423897fff0963bb71cc02ba648ca2c6c6764
Charles D'Aoust <cdaoust@home.forked.io>
2020-10-05 04:06:02 -0400

    Arguments!

--- main.py
+++ main.py
@@ -2,86 +2,144 @@ import pyaudio
 import numpy as np
 import sacn
 import time
-import signal
-import os
-from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import wx
+import argparse
 
-maxValue = 2**16
+maxValue = 2 ** 16
 pixels = 110
 bars = 35
 defaultframes = 512
 
 
+def senderSetup(ip):
+    sender = sacn.sACNsender()
+    sender.start()
+    sender[1].destination = ip
+    sender.activate_output(1)
+    return sender
 
-#Use module
-p = pyaudio.PyAudio()
 
-device_id = 17
-
-
-#Open stream
-stream = p.open(format = pyaudio.paInt16,
-                channels = 2,
-                rate = 48000,
-                input = True,
-                frames_per_buffer = defaultframes,
-                input_device_index = 17,
-                as_loopback = True)
-
-sender = sacn.sACNsender()  # provide an IP-Address to bind to if you are using Windows and want to use multicast
-sender.start()  # start the sending thread
-sender.activate_output(1)  # start sending out data in the 1st universe
-
-sender[1].destination = "10.2.20.240"
-
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
+def parse_args(choices):
+    parser = argparse.ArgumentParser(
+        description="DMX WiFi Sender help", prog="DMX WiFi Sender"
+    )
+    parser.add_argument("--ip", help="Destination DMX server")
+    parser.add_argument("--id", help="Index of the soundcard")
+    parser.add_argument("--list", help="List available soundcards", action="store_true")
+    return parser.parse_args()
 
 
 def buildDMX(dataL, dataR):
-    volumelvl = volume.GetMasterVolumeLevel()
-    peakL = np.abs(np.max(dataL)-np.min(dataL))/maxValue*pixels*1.5
-    peakR = np.abs(np.max(dataR)-np.min(dataR))/maxValue*pixels*1.5
+    peakL = np.abs(np.max(dataL) - np.min(dataL)) / maxValue * pixels * 1.5
+    peakR = np.abs(np.max(dataR) - np.min(dataR)) / maxValue * pixels * 1.5
     dmxL = ()
     dmxR = ()
-    for i in range(1, int(pixels/2)):
-        if i <= int(peakL) and i < int(pixels/6):
-            dmxL = dmxL + (0,127,0) # reversed color
-        elif i <= int(peakL) and i > (pixels/6) and i < (pixels/3) :
-            dmxL = dmxL + (0,127,127) # reversed color
-        elif i <= int(peakL) and i > (pixels/3):
-            dmxL = dmxL + (0,0,127) # reversed color
+    for i in range(1, int(pixels / 2)):
+        if i <= int(peakL) and i < int(pixels / 6):
+            dmxL = dmxL + (0, 255, 0)  # reversed color
+        elif i <= int(peakL) and i > (pixels / 6) and i < (pixels / 3):
+            dmxL = dmxL + (0, 255, 255)  # reversed color
+        elif i <= int(peakL) and i > (pixels / 3):
+            dmxL = dmxL + (0, 0, 255)  # reversed color
         else:
-            dmxL = dmxL + (0,0,0)
-        if i <= int(peakR) and i < int(pixels/6):
-            dmxR = dmxR + (0,127,0)
-        elif i <= int(peakR) and i > (pixels/6) and i < (pixels/3) :
-            dmxR = dmxR + (127,127,0)
-        elif i <= int(peakR) and i > (pixels/3):
-            dmxR = dmxR + (127,0,0)
+            dmxL = dmxL + (0, 0, 0)
+        if i <= int(peakR) and i < int(pixels / 6):
+            dmxR = dmxR + (0, 255, 0)
+        elif i <= int(peakR) and i > (pixels / 6) and i < (pixels / 3):
+            dmxR = dmxR + (255, 255, 0)
+        elif i <= int(peakR) and i > (pixels / 3):
+            dmxR = dmxR + (255, 0, 0)
     dmxL = dmxL[::-1]
     dmx = dmxL + dmxR
+    return dmx
 
-    return(dmx)
 
-def main():
+def startLED(deviceid, loopback, channels, sampleRate):
+    stream = p.open(
+        format=pyaudio.paInt16,
+        channels=int(channels),
+        rate=int(sampleRate),
+        input=True,
+        frames_per_buffer=defaultframes,
+        input_device_index=int(deviceid),
+        as_loopback=loopback,
+    )
     while True:
-        data = np.fromstring(stream.read(1024),dtype=np.int16)
+        data = np.frombuffer(stream.read(1024), dtype=np.int16)
         dataL = data[0::2]
         dataR = data[1::2]
-        time.sleep(0.01)
         (dmx) = buildDMX(dataL, dataR)
         sender[1].dmx_data = dmx
-      
+        time.sleep(0.03)
 
 
-if __name__ == '__main__':
+# devices = AudioUtilities.GetSpeakers()
+# interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+# volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+
+def stopLED():
+    sender.stop()
+
+
+def get_soundcards(p):
+    soundcards = {}
+    for i in range(0, p.get_device_count()):
+        info = p.get_device_info_by_index(i)
+        if p.get_host_api_info_by_index(info["hostApi"])["index"] == 1:
+            soundcards[i] = {
+                "name": p.get_device_info_by_index(i)["name"],
+                "outChannels": p.get_device_info_by_index(i)["maxOutputChannels"],
+                "inChannels": p.get_device_info_by_index(i)["maxInputChannels"],
+                "sampleRate": p.get_device_info_by_index(i)["defaultSampleRate"],
+            }
+            soundcards["default"] = p.get_host_api_info_by_index(info["hostApi"])[
+                "defaultOutputDevice"
+            ]
+    return soundcards
+
+
+if __name__ == "__main__":
+    p = pyaudio.PyAudio()
+    soundcardlist = get_soundcards(p)
+    args = parse_args(soundcardlist)
+    if args.list is True:
+        for i in soundcardlist:
+            if not i == "default":
+                if i == soundcardlist["default"]:
+                    print(i, soundcardlist[i]["name"], "[DEFAULT]")
+            else:
+                print(i, soundcardlist[i]["name"])
+                quit()
+    elif args.ip is None:
+        print("IP address required, use --help")
+        quit()
     try:
-        main()
+        if args.id is None:
+            deviceid = soundcardlist["default"]
+        else:
+            deviceid = int(args.id)
+        try:
+            sender = sacn.sACNsender()
+            sender.start()
+            sender.activate_output(1)
+            sender[1].destination = str(args.ip)
+        except Exception as e:
+            print(e)
+            sender.stop()
+        except:
+            sender.stop()
+        if soundcardlist[deviceid]["outChannels"] > 0:
+            loopback = True
+        else:
+            loopback = False
+        startLED(
+            deviceid,
+            loopback,
+            soundcardlist[deviceid]["outChannels"],
+            soundcardlist[deviceid]["sampleRate"],
+        )
     except Exception as e:
         print(e)
         sender.stop()
