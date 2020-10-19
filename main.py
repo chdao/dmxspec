0c257dd59d46eec92eec53c84be85876350953ae
wickedsun <charles.daoust@gmail.com>
2020-10-18 21:21:53 -0400

    Switched to SoundCard library instead of pyAudio

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pyaudio
 import numpy as np
 import sacn
 import time
 import sys
+import soundcard as sc
 
 # from comtypes import CLSCTX_ALL
 # from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
@@ -42,7 +42,15 @@ def parse_args(choices):
 
 
 class BuildDMX:
-    def __init__(self, pixels, fps, brightness, multi, reverse_right, reverse_left):
+    def __init__(
+        self: object,
+        pixels: int,
+        fps: int,
+        brightness: int,
+        multi: float,
+        reverse_right: bool,
+        reverse_left: bool,
+    ):
         self.channel_size = pixels // 2
         self.section_size = self.channel_size / 6
         self.maxValue = 2 ** 16
@@ -56,7 +64,7 @@ class BuildDMX:
         # to match the data taken from pyaudio.
         self.channel_order = {0: reverse_left, 1: reverse_right}
 
-    def build_rgb(self, channel, peak, previous_dmx=None):
+    def build_rgb(self: object, peak: float, previous_dmx: dict = None):
         dmx = {}
         # Get the peak (volume) value of the channel
         for i in range(self.channel_size):
@@ -64,7 +72,7 @@ class BuildDMX:
                 division = int(i + 1 // self.section_size + 1)
             else:
                 division = int(i // self.section_size + 1)
-            if int(peak) >= i + 0.01:
+            if int(peak) >= i + 0.1:
                 # Figure out what the gradient value is for color transitions.
                 # Basically a % of the position in each subdivision of the
                 # channel.
@@ -119,45 +127,51 @@ class BuildDMX:
                     dmx[i] = {"r": 0, "g": 0, "b": 0}
         return dmx
 
-    def output(self, data, previous_dmx):
+    def output(self: object, data: list, previous_dmx: dict):
         dmx_data = {}
         output_data = []
+
         for channel in range(0, 2):
-            peak = (
-                int(np.abs(np.max(data[channel::2])) - int(np.min(data[channel::2])))
-                / self.maxValue
-                * self.pixels
-                * float(self.multi)
-            )
-            # Send previous dmx data. If this is the first rul, it'll raise LookupError.
+            # Send previous dmx data. If this is the first run, it'll raise
+            # a LookupError.
+            peak = self.get_peak(data, channel)
             try:
-                dmx_data[channel] = self.build_rgb(channel, peak, previous_dmx[channel])
+                dmx_data[channel] = self.build_rgb(peak, previous_dmx[channel])
             except LookupError:
-                dmx_data[channel] = self.build_rgb(channel, peak)
-        for i in dmx_data:
-            if self.channel_order[i] is True:
-                for j in range(len(dmx_data[i]) - 1, -1, -1):
+                dmx_data[channel] = self.build_rgb(peak)
+            if self.channel_order[channel] is True:
+                for j in range(len(dmx_data[channel]) - 1, -1, -1):
                     for c in ("r", "g", "b"):
                         # Create a list of all the LEDs from the dmx_data
-                        output_data.append(dmx_data[i][j][c])
+                        output_data.append(dmx_data[channel][j][c])
             else:
-                for j in dmx_data[i]:
+                for j in dmx_data[channel]:
                     for c in ("r", "g", "b"):
                         # Create a list of all the LEDs from the dmx_data
-                        output_data.append(dmx_data[i][j][c])
+                        output_data.append(dmx_data[channel][j][c])
 
         # Change the list to a tuple for the dmx library
         return (tuple(output_data), dmx_data)
 
+    def get_peak(self, data, channel: int):
+        peak = int
+        current_channel = []
+        for i in data:
+            current_channel.append(i[channel])
+        peak = (
+            np.abs(np.max(current_channel) - np.min(current_channel))
+            * self.pixels
+            * float(self.multi)
+        )
+        return peak
+
 
 def start_sequence(
     deviceid: int,
-    loopback: bool,
     channels: int,
     sampleRate: int,
     fps: int,
     brightness: int,
-    p: object,
     defaultframes: int,
     pixels: int,
     multi: float,
@@ -165,24 +179,17 @@ def start_sequence(
     rl: bool,
     ip: str,
 ) -> object:
-    stream = p.open(
-        format=pyaudio.paInt16,
-        channels=int(channels),
-        rate=int(sampleRate),
-        input=True,
-        frames_per_buffer=defaultframes,
-        input_device_index=int(deviceid),
-        as_loopback=loopback,
-    )
+    recording_device = sc.get_microphone(sc.default_speaker().id, include_loopback=True)
     sender = sacn.sACNsender()
     sender.start()
     sender.activate_output(1)
     sender[1].destination = ip
     dmx = BuildDMX(pixels, fps, brightness, multi, rr, rl)
     previous_dmx = {}
+
     try:
         while True:
-            data = np.frombuffer(stream.read(1024), dtype=np.int16)
+            data = recording_device.record(samplerate=48000, numframes=512)
             (dmx_data, previous_dmx) = dmx.output(data, previous_dmx)
             sender[1].dmx_data = dmx_data
             terminal_led(dmx_data)
@@ -205,38 +212,39 @@ def terminal_led(dmx_data):
 # volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 
-def get_soundcards(p):
-    soundcards = {}
-    for i in range(0, p.get_device_count()):
-        info = p.get_device_info_by_index(i)
-        if p.get_host_api_info_by_index(info["hostApi"])["index"] == 1:
-            soundcards[i] = {
-                "name": p.get_device_info_by_index(i)["name"],
-                "outChannels": p.get_device_info_by_index(i)["maxOutputChannels"],
-                "inChannels": p.get_device_info_by_index(i)["maxInputChannels"],
-                "sampleRate": p.get_device_info_by_index(i)["defaultSampleRate"],
-            }
-            soundcards["default"] = p.get_host_api_info_by_index(info["hostApi"])[
-                "defaultOutputDevice"
-            ]
-    return soundcards
+# def get_soundcards(p):
+#    soundcards =
+#    for i in range(0, p.get_device_count()):
+#        info = p.get_device_info_by_index(i)
+#        if p.get_host_api_info_by_index(info["hostApi"])["index"] == 1:
+#            soundcards[i] = {
+#                "name": p.get_device_info_by_index(i)["name"],
+#                "outChannels": p.get_device_info_by_index(i)["maxOutputChannels"],
+#                "inChannels": p.get_device_info_by_index(i)["maxInputChannels"],
+#                "sampleRate": p.get_device_info_by_index(i)["defaultSampleRate"],
+#            }
+#            soundcards["default"] = p.get_host_api_info_by_index(info["hostApi"])[
+#                "defaultOutputDevice"
+#            ]
+#    return soundcards
 
 
 def main():
-    p = pyaudio.PyAudio()
-    soundcardlist = get_soundcards(p)
+    soundcardlist = sc.all_microphones(include_loopback=True)
     args = parse_args(soundcardlist)
     defaultframes = int(args.frames)
     pixels = int(args.pixels)
     fps = int(args.fps)
     brightness = args.brightness
     if args.list is True:
-        for i in soundcardlist:
-            if not i == "default":
-                if i == soundcardlist["default"]:
-                    print(i, soundcardlist[i]["name"], "[DEFAULT]")
-                else:
-                    print(i, soundcardlist[i]["name"])
+        i = 0
+        print("Default\t| Index\t| Name\n" + "-" * 50)
+        while i < len(soundcardlist):
+            if soundcardlist[i].id == sc.default_speaker().id:
+                print("   X\t|", str(i) + "\t|", soundcardlist[i].name)
+            else:
+                print("\t|", str(i) + "\t|", soundcardlist[i].name)
+            i += 1
         raise Exception
     elif args.ip is None:
         print("IP address required, use --help")
@@ -245,24 +253,16 @@ def main():
         print("Brightness cannot be above 100%")
         raise Exception
     if args.id is None:
-        deviceid = soundcardlist["default"]
+        deviceid = str(sc.default_speaker().id)
     else:
         deviceid = int(args.id)
 
-    if soundcardlist[deviceid]["outChannels"] > 0:
-        loopback = True
-        channels = soundcardlist[deviceid]["outChannels"]
-    else:
-        loopback = False
-        channels = soundcardlist[deviceid]["inChannels"]
     start_sequence(
         deviceid,
-        loopback,
-        channels,
-        soundcardlist[deviceid]["sampleRate"],
+        2,
+        48000,
         fps,
         brightness,
-        p,
         defaultframes,
         pixels,
         args.multi,
