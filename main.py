58ebf81ccc2f6dbcdfd161516176040a14a66108
wickedsun <charles.daoust@gmail.com>
2020-10-21 02:43:17 -0400

    added block size, removed numframes and reduced sampling of the peak.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Hello!
+This software creates a VU meter and sends it to a DMX enabled (WiFi) LED strip.
 """
 import time
 import argparse
@@ -28,10 +28,10 @@ def parse_args(choices):
     parser.add_argument("--rl", help="Reverse Left Channel", action="store_true")
     parser.add_argument("-p", "--pixels", type=int, help="Length of strip", default=100)
     parser.add_argument(
-        "-f", "--frames", type=int, help="Frames for pyAudio", default=256
+        "-f", "--frames", type=int, help="Frames for pyAudio", default=512
     )
     parser.add_argument(
-        "--fps", type=int, help="Frame Per Second (refresh rate)", default=30
+        "--fps", type=int, help="Frame Per Second (refresh rate)", default=100
     )
     parser.add_argument(
         "-b",
@@ -56,23 +56,21 @@ class BuildDMX:
     ):
         self.channel_size = pixels // 2
         self.section_size = self.channel_size / 6
-        self.maxValue = 2 ** 16
+        self.maxvalue = 2 ** 16
         self.fps = fps
         self.brightness = brightness
         self.multi = multi
         self.reverse_right = reverse_right
         self.reverse_left = reverse_left
         self.pixels = pixels
-        self.fade_multiplier = 1.1
+        self.fade_multiplier = 1.2
         # Mapping channels to numbers, this is
         # to match the data taken from pyaudio.
         self.channel_order = {0: reverse_left, 1: reverse_right}
 
     def build_rgb(self, peak: float, previous_dmx: dict = None):
         """
-
-        bite
-
+        Build the RGB sequence based on the peak.
         """
         dmx = {}
         # Get the peak (volume) value of the channel
@@ -123,16 +121,13 @@ class BuildDMX:
                     dmx[i] = {
                         # Decay the LEDs off, makes transitions smoother
                         "r": int(
-                            (previous_dmx[i]["r"] / self.fps)
-                            * (self.fps // self.fade_multiplier)
+                            (previous_dmx[i]["r"] / self.fade_multiplier)
                         ),
                         "g": int(
-                            (previous_dmx[i]["g"] / self.fps)
-                            * (self.fps // self.fade_multiplier)
+                            (previous_dmx[i]["g"] / self.fade_multiplier)
                         ),
                         "b": int(
-                            (previous_dmx[i]["b"] / self.fps)
-                            * (self.fps // self.fade_multiplier)
+                            (previous_dmx[i]["b"] / self.fade_multiplier)
                         ),
                     }
                     # If the brightness is under 1, turn off completely.
@@ -173,9 +168,13 @@ class BuildDMX:
         return (tuple(output_data), dmx_data)
 
     def get_peak(self, data, channel: int):
+        """
+        Get 1/4 of all the sound data and find the peak.
+        """
         peak = int
         current_channel = []
-        for i in data:
+        values = data[::10]
+        for i in values:
             current_channel.append(i[channel])
             peak = (
                 np.abs(np.max(current_channel) - np.min(current_channel))
@@ -198,6 +197,9 @@ def start_sequence(
     rl: bool,
     ip: str,
 ) -> object:
+    """
+    Main sequence
+    """
     recording_device = sc.get_microphone(sc.default_speaker().id, include_loopback=True)
     sender = sacn.sACNsender()
     sender.start()
@@ -208,25 +210,31 @@ def start_sequence(
 
     try:
         while True:
-            data = recording_device.record(samplerate=48000, numframes=defaultframes)
+            data = recording_device.record(samplerate=48000, numframes=None, blocksize=512)
             (dmx_data, previous_dmx) = dmx.output(data, previous_dmx)
             sender[1].dmx_data = dmx_data
             terminal_led(dmx_data)
-            time.sleep(1 // fps)
+            time.sleep(0.001)
     except KeyboardInterrupt:
         sender.stop()
         cursor.show()
 
 
 def terminal_led(dmx_data):
+    """
+    Send the LED sequence to the terminal for emulation
+    """
     cursor.hide()
     vumeter = ""
     for i in range(0, len(dmx_data), 3):
-        vumeter = vumeter + color("#", fore=(dmx_data[i: i + 3]))
+        vumeter = vumeter + color("█", fore=(dmx_data[i: i + 3]))
     print("\r[" + vumeter + "]", end="")
 
 
 def main():
+    """
+    Main program
+    """
     soundcardlist = sc.all_microphones(include_loopback=True)
     args = parse_args(soundcardlist)
     defaultframes = int(args.frames)
